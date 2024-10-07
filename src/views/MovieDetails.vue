7e1f0d84669014e1a5e90f563cea210f1f7b5e76
AJ0267 <aj0386057@gmail.com>
2024-10-07 13:46:41 +0530

    new commit

--- src/views/MovieDetails.vue
+++ src/views/MovieDetails.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="max-w-4xl mx-auto p-6">
+    <h1 class="text-4xl font-bold mb-4 text-center">{{ movie.Title }}</h1>
     <div class="flex flex-col md:flex-row md:items-center">
       <img
         :src="movie.Poster"
         alt="Movie Poster"
         class="w-full md:w-2/3 lg:w-3/5 xl:w-1/2 h-auto object-cover rounded-lg shadow-lg mb-4 md:mb-0 md:mr-4"
       />
-      <h1 class="text-4xl font-bold mb-4 text-center">{{ movie.Title }}</h1>
       <div class="md:ml-4">
         <p class="text-lg"><strong>Released:</strong> {{ movie.Released }}</p>
         <p class="text-lg"><strong>Genre:</strong> {{ movie.Genre }}</p>
@@ -32,12 +32,12 @@ export default {
     };
   },
   created() {
-    const title = this.$route.params.title; 
+    const title = this.$route.params.title;
     axios
       .get(`https://www.omdbapi.com/?t=${title}&apikey=${this.apiKey}`)
       .then((response) => {
         if (response.data.Response === "True") {
-          this.movie = response.data; 
+          this.movie = response.data;
         } else {
           console.error("Error:", response.data.Error);
         }
